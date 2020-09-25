1c0d49c3fad63ecd7977b85a63f0c7dfa62558fd
JayTalekar <jaytalekar.developer@gmail.com>
2020-09-25 22:40:11 +0530

    Added Filter to Show Rental and Sale Properties.

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -21,12 +21,15 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val BASE_URL = "https://mars.udacity.com/"
+
+enum class MarsApiFilter(val value : String)
+        { SHOW_RENT("rent"), SHOW_BUY("buy"), SHOW_ALL("all")}
 
 private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
@@ -40,7 +43,7 @@ private val retrofit = Retrofit.Builder()
 
 interface MarsApiService{
     @GET("realestate")
-    fun getProperties() : Deferred<List<MarsProperty>>
+    fun getProperties(@Query("filter") type : String) : Deferred<List<MarsProperty>>
 }
 
 object MarsApi{
